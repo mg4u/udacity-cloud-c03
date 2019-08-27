44931d10c34765078bab82719db2745463ea3c0c
Mike Bromberek <mikebromberek@gmail.com>
2019-08-26 19:36:20 -0500

    Split out the services for User and Feed into separate folders and services that will listen on different ports

---
+++ exercises/udacity-c3-feed-service/src/controllers/v0/feed/models/FeedItem.ts
@@ -0,0 +1,20 @@
+import {Table, Column, Model, HasMany, PrimaryKey, CreatedAt, UpdatedAt, ForeignKey} from 'sequelize-typescript';
+import { User } from '../../../../../../udacity-c3-restapi/src/controllers/v0/users/models/User';
+// import { requireAuth } from '../../../../../../udacity-c3-restapi/src/controllers/v0/users/routes/auth.router';
+
+@Table
+export class FeedItem extends Model<FeedItem> {
+  @Column
+  public caption!: string;
+
+  @Column
+  public url!: string;
+
+  @Column
+  @CreatedAt
+  public createdAt: Date = new Date();
+
+  @Column
+  @UpdatedAt
+  public updatedAt: Date = new Date();
+}
